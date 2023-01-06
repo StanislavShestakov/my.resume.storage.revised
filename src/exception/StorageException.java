c4cdb21f276ba3ed4f63d393a4fe29dc0b8da7ab
Shestakov Stanislav <w3orks@mail.ru>
2023-01-06 19:30:23 +0300

    HW 8 Переделать модель резюме: учесть, что на одной работе (в одном учебном заведении) можно работать/ учиться в разные периоды и при этом имя организации не дублируется Сделать рекурсивный обход и вывод имени файлов в каталогах и подкаталогах (корневой каталог- ваш проект) Заполнить в AbstractStorageTest резюме данными Реализовать AbstractFileStorage, базовый класс для хранения резюме в файлах.

--- src/exception/StorageException.java
+++ src/exception/StorageException.java
@@ -1,12 +1,20 @@
 package exception;
 
-public class StorageException extends RuntimeException{
+import java.io.IOException;
+
+public class StorageException extends RuntimeException {
     private final String uuid;
 
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
     }
+
+    public StorageException(String message, String uuid, Exception e) {
+        super(message, e);
+        this.uuid = uuid;
+    }
+
 
     public String getUuid() {
         return uuid;
