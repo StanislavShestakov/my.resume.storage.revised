8e571b6bbf6cec601b1be8ec05b44d4d7fb35b59
Shestakov Stanislav <w3orks@mail.ru>
2023-01-06 23:14:20 +0300

    HW 9 Сделать рекурсивный вывод каталогов с отступом Сделать возможным выбор меджу FileStorage и PathStorage для сериализации через ObjectStream.

--- test/storage/AllStorageTest.java
+++ test/storage/AllStorageTest.java
@@ -12,8 +12,8 @@ import org.junit.runners.Suite;
                 ListStorageTest.class,
                 MapUuidStorageTest.class,
                 MapResumeStorageTest.class,
-                ObjectStreamStorageTest.class,
-                ObjectStreamPathStorageTest.class
+                ObjectFileStorageTest.class,
+                ObjectPathStorageTest.class
         })
 public class AllStorageTest {
 }
