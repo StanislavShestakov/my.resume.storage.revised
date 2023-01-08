e4362ed25ab66ab64a759f4289616b8f50fae4f8
Shestakov Stanislav <w3orks@mail.ru>
2023-01-08 18:43:50 +0300

    HW 11 Реализовать deadlock Взаимная блокировка

--- src/Loader.java
+++ src/Loader.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 public class Loader {
-    private final static Storage ARRAY_STORAGE = new ArrayStorage();
+    private final  static Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
