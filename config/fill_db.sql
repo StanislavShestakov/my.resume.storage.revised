40362a9dc947bd75bd9a045b2630398860eca447
Shestakov Stanislav <w3orks@mail.ru>
2023-03-17 18:12:21 +0300

    HW13 Implement SqlStorage

--- config/fill_db.sql
+++
@@ -1,2 +0,0 @@
-insert into resume (uuid, full_name) VALUES (1,'Vasya Pupkin');
-insert into contact (type, value, resume_uuid) values ('phone', '0713824572',1)
--- src/exception/StorageException.java
+++ src/exception/StorageException.java
@@ -14,6 +14,10 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
+    }
+
     public StorageException(String message, Exception e) {
         this(message, null, e);
     }
@@ -22,10 +26,7 @@ public class StorageException extends RuntimeException {
         super(message, e);
         this.uuid = uuid;
     }
-    public StorageException(Exception e){
-        this(e.getMessage(),null,e);
 
-    }
     public String getUuid() {
         return uuid;
     }
